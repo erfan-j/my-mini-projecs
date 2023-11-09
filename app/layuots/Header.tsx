e8f80948a2feb554e1b73bd0e171a05c3e2b6eda
Erfan <erfan.jahandoost84@gmail.com>
2023-11-09 23:21:40 +0330

    feat:Header Created.

--- app/layuots/Header.tsx
+++ app/layuots/Header.tsx
@@ -1,3 +1,17 @@
+import DropDown from "../components/shared/DropDown";
+
 export default function Header() {
-  return <div> this is Header.</div>;
+  return (
+    <section className=" border-primary border-t ">
+      <div className="flex flex-row-reverse lg:flex-row py-4 px-8 items-center justify-center lg:justify-between">
+        <span className="text-xl font-bold hidden lg:block">
+          Welcome to my miniProjects menu!
+        </span>
+        <div>
+          <DropDown />
+        </div>
+        <span className="text-lg hidden lg:block">erfan.jahandoost84@gmail.com</span>
+      </div>
+    </section>
+  );
 }
